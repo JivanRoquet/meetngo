de79fd3944f228705bce6158700c62dd67a4354d
Jivan Roquet <jivan.roquet@gmail.com>
2014-09-14 15:18:17 +0200

    Updates

--- meetngo/utils.py
+++ meetngo/utils.py
@@ -6,7 +6,7 @@ import json
 
 def load_events_file():
     path = meetngo.__path__[0]
-    events_file = os.path.join(path, 'data', 'events.json')
-    events_file_content = open(events_file)
+    events_file = os.path.join(path, 'data', 'events_old.json')
+    events_file_content = open(events_file, encoding='utf-8')
     events = json.loads(events_file_content.read())
     return events
